c747c8d730ad3b3d3270f771b8f73a729ca8a8fb
342268316 <342268316@qq.com>
2019-10-07 08:36:54 +0800

    up0304

---
+++ 03-继承/demo01/Extends/Private/SubClass.java
@@ -0,0 +1,7 @@
+package Extends.Private;
+
+public class SubClass extends SuperClass {
+    public void m3() {
+        System.out.println("SubClass里面m3方法");
+    }
+}
---
+++ 03-继承/demo02/Super/Animal.java
@@ -0,0 +1,8 @@
+package Super;
+
+public class Animal {
+    public String name="动物";
+    public void eat(){
+        System.out.println("动物吃饭");
+    }
+}
