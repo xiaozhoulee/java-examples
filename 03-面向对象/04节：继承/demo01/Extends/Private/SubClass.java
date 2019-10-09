cc0b29ccb47265733ed960f3f39060cdfb7e7d4f
342268316 <342268316@qq.com>
2019-10-09 09:43:03 +0800

    up0602

--- 03-面向对象/04节：继承/demo01/Extends/Private/SubClass.java
+++
@@ -1,7 +0,0 @@
-package Extends.Private;
-
-public class SubClass extends SuperClass {
-    public void m3() {
-        System.out.println("SubClass里面m3方法");
-    }
-}
--- 03-面向对象/04节：继承/demo02/Super/Animal.java
+++
@@ -1,8 +0,0 @@
-package Super;
-
-public class Animal {
-    public String name="动物";
-    public void eat(){
-        System.out.println("动物吃饭");
-    }
-}
