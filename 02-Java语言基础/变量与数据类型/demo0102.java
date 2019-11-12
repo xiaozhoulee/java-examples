fd54895258078da520dfc100bf58fc3b829f72bf
unknown <342268316@qq.com>
2019-11-12 17:08:50 +0800

    up0102

---
+++ 02-Java语言基础/变量与数据类型/demo0102.java
@@ -0,0 +1,31 @@
+package com.xiaozhoubg;
+
+public class demo0102 {
+    public static void main(String[] args) {
+        byte a1 = 67;
+        short a2 = 10000;
+        int a3 = 1000000;
+        long a4 =99999999;
+
+        double a5 = 3.14;
+        float  a6=  1.2F;
+
+        char a7 = 'a';
+        char a8 = '小';
+
+        boolean a9 = true;
+        
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a7);
+        System.out.println(a8);
+        System.out.println(a9);
+
+    }
+
+
+}
