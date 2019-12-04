fb2dbd381a250c06dd9e1a6c02cf907458a17aed
unknown <342268316@qq.com>
2019-12-04 16:23:44 +0800

    up1402demo

---
+++ 14-spring_mvc/第02节：maven管理springMVC/springmvc01/src/main/java/com/xiaozhoubg/ControllerTest01.java
@@ -0,0 +1,14 @@
+package com.xiaozhoubg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//控制器类
+@Controller
+public class ControllerTest01 {
+    @RequestMapping(path = "/hello")
+    public String test01(){
+        System.out.println("hello");
+        return "success";
+    }
+}
